b411c83cfdd6051f23b35b0c73f7a4e7128f58f4
Shashank-09 <shashanktulsikar979@gmail.com>
2024-03-04 22:44:17 +0530

    models

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,8 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs'
+          
+cloudinary.config({ 
+  cloud_name: 'dvr1rap7t', 
+  api_key: '435346541625971', 
+  api_secret: 'A9mxWhs5Lu5T3XsT16hMiVTVaTA' 
+});
